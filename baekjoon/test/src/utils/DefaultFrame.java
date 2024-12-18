b871b6d79686d4cf14e1feed2d95ae7d48719d57
shurona <khc4669@naver.com>
2024-12-18 16:27:31 +0900

    fix: 백준 기본 프레임 수정

--- baekjoon/test/src/utils/DefaultFrame.java
+++ baekjoon/test/src/utils/DefaultFrame.java
@@ -1,10 +1,12 @@
 package utils;
 
+import java.io.IOException;
+
 public class DefaultFrame {
 
     public class Main {
 
-        public static void main(String[] args) {
+        public static void main(String[] args) throws IOException {
             //
         }
     }
